eeb32017ac24101c87f5c8f7b3eb68707cfded02
Shantaram K <shantaramk@mipladmin-67.local>
2019-09-20 12:24:54 +0530

    Initial Commit

---
+++ UITestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITestDemo
+//
+//  Created by Shantaram K on 20/09/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
