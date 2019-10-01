72357270123ffe0eb96c867c6a469c2461981100
Shantaram K <shantaramk@Mipladmin-67.local>
2019-10-01 14:38:54 +0530

    first commit

---
+++ UITestDemo/Assets.xcassets/JSONFel.swift
@@ -0,0 +1,13 @@
+//
+//  JSONFel.swift
+//  UITestDemo
+//
+//  Created by Shantaram K on 20/09/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class JSONFel: NSObject {
+
+}
---
+++ UITestDemo/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  UITestDemo
+//
+//  Created by Shantaram K on 20/09/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
